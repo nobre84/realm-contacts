66b70fbcb5dd2754851e393e2502531d8b627a09
Rafael Nobre <nobre84@gmail.com>
2019-04-06 09:22:03 -0300

    Reload after insert

--- CT Rafael Nobre/Sources/Controller/ContactListController.swift
+++ CT Rafael Nobre/Sources/Controller/ContactListController.swift
@@ -117,8 +117,7 @@ class ContactListController: UITableViewController {
     // MARK: - Actions
     
     @IBAction func unwindToContactList(_ unwindSegue: UIStoryboardSegue) {
-        let sourceViewController = unwindSegue.source
-        // Use data from the view controller which initiated the unwind segue
+        fetch()
     }
 
 }
